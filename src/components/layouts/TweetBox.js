6a695f3eaa55bb9008ba75298d7b79567ee14a61
Ernesto Custodio <ernestojosecg@gmail.com>
2020-10-14 11:25:31 +0200

    Style

--- src/components/layouts/TweetBox.js
+++ src/components/layouts/TweetBox.js
@@ -1,29 +1,48 @@
 import React from 'react'
 import "./Feed.css"
-import { Avatar, Button} from "@material-ui/core"
+import { Avatar, Button, IconButton} from "@material-ui/core"
 import WallpaperIcon from '@material-ui/icons/Wallpaper';
+import GifIcon from '@material-ui/icons/Gif';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import EventIcon from '@material-ui/icons/Event';
 
 function TweetBox() {
     return (
         <div className="tweetbox">
-               <Avatar />
-            <form>
+            <Avatar className="tweetbox_avatar"/>
+            <form className="tweetbox_form">
                 <div className="tweetbox_input">
                  
                     <input placeholder="¿Qué está pasando?" type="text" />
                    
                 </div>
-                <div> 
-                    <label htmlFor="file-upload" className="label-file">
-              
-                    <WallpaperIcon />
-                    </label>
-                    <input
-                        id="file-upload"
-                        type="file"
-                        className="inputFile"
-                    />
-                    <Button> Tweet</Button>
+                <div className="tweetbox_bottom"> 
+                    <div className="tweetbox_options">
+                        <label htmlFor="file-upload" className="label-file">
+                        <IconButton>
+                        <WallpaperIcon />
+                        </IconButton>
+                        </label>
+                        <input
+                            id="file-upload"
+                            type="file"
+                            className="inputFile"
+                        />
+                        <IconButton>
+                            <GifIcon />
+                        </IconButton>
+                        <IconButton>
+                            <EqualizerIcon />
+                        </IconButton>
+                        <IconButton>
+                            <InsertEmoticonIcon />
+                        </IconButton>
+                        <IconButton>
+                            <EventIcon />
+                        </IconButton>
+                    </div>
+                    <Button className="tweetbox_button"> Twittear</Button>
                 </div>
                
             </form>
