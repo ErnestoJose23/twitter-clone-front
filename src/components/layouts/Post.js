6bd71ca288776d7aea304987b41b379fb9dbb5e8
Ernesto Custodio <ernestojosecg@gmail.com>
2020-10-19 12:57:18 +0200

    Pusher

--- src/components/layouts/Post.js
+++ src/components/layouts/Post.js
@@ -14,6 +14,7 @@ function Post({user_id, timestamp, tweet, imagename}) {
     const [displayName, setDisplayName] = useState();
     const [avatar, setAvatar] = useState();
     const [verified, setVerified] = useState(false);
+    const [timesince, setTimesince] = useState();
     useEffect(() => {
 
         const fetchPosts = async () => 
@@ -24,9 +25,46 @@ function Post({user_id, timestamp, tweet, imagename}) {
             setAvatar(response.data.avatar);
             setVerified(response.data.verified)
           });
-
+          setTimesince(timeDifference(Date.now(), timestamp));
           fetchPosts();
     }, [])
+
+
+    function timeDifference(current, previous) {
+
+        var msPerMinute = 60 * 1000;
+        var msPerHour = msPerMinute * 60;
+        var msPerDay = msPerHour * 24;
+        var msPerMonth = msPerDay * 30;
+        var msPerYear = msPerDay * 365;
+    
+        var elapsed = current - previous;
+    
+        if (elapsed < msPerMinute) {
+             return Math.round(elapsed/1000) + 'secs ';   
+        }
+    
+        else if (elapsed < msPerHour) {
+             return Math.round(elapsed/msPerMinute) + 'min ';   
+        }
+    
+        else if (elapsed < msPerDay ) {
+             return Math.round(elapsed/msPerHour ) + 'hours ';   
+        }
+    
+        else if (elapsed < msPerMonth) {
+            return  Math.round(elapsed/msPerDay) + 'days ';   
+        }
+    
+        else if (elapsed < msPerYear) {
+            return  Math.round(elapsed/msPerMonth) + 'months ';   
+        }
+    
+        else {
+            return  Math.round(elapsed/msPerYear ) + 'years ';   
+        }
+    }
+
     return (
         <div className="post">
             <div className="post_avatar">
@@ -35,7 +73,7 @@ function Post({user_id, timestamp, tweet, imagename}) {
             <div className="post_body">
                 <div className="post_header">
                     <div className="post_headerText">
-                        <h3>{displayName} <span className="post_headerSpecial">{verified && <VerifiedUserIcon className="post_badge"/>} @{username} - {timestamp}
+                        <h3>{displayName} <span className="post_headerSpecial">{verified && <VerifiedUserIcon className="post_badge"/>} @{username} - {timesince}
                         </span></h3>
                     </div>
                     <div className="post_headerDescription">
