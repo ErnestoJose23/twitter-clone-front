a5c2287bf9c557535763d68608e0239bac946bc5
Ernesto Custodio <ernestojosecg@gmail.com>
2020-10-20 12:47:45 +0200

    Profile

--- src/components/layouts/ProfileLayout.js
+++ src/components/layouts/ProfileLayout.js
@@ -3,10 +3,38 @@ import UserContext from "../../context/UserContext";
 import './Profile.css'
 import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
 import { Avatar, Button, IconButton} from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 2, 3),
+    },
+  }));
 
 
 function ProfileLayour() {
     const { userData, setUserData } = useContext(UserContext);
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+    
 
     var sectionStyle = {
         width: "100%",
@@ -14,6 +42,16 @@ function ProfileLayour() {
         backgroundImage: "url(https://sc2.elpais.com.uy/files/image_1254_485/uploads/2018/04/11/5ace0d44b87c3.jpeg)"
       };
       
+
+    const body = (
+    <div  className={classes.paper}>
+        <h2 id="simple-modal-title">Text in a modal</h2>
+        <p id="simple-modal-description">
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </p>
+        
+    </div>
+    );
     return (
         <div className="profile">
             <div className="profile_header">
@@ -22,7 +60,24 @@ function ProfileLayour() {
             </div>
             <div className="profile_usercard">
                 {userData.cover ? (<div></div>) : (<div className="profile_cover" style={ sectionStyle }></div>)}
-                <Avatar className="profile_avatar" src={userData.avatar} />
+                <div className="profile_flex_avatar">
+                    <Avatar className="profile_avatar" src={userData.avatar} />
+                    <Button className="profile_editButton" onClick={handleOpen}>Editar Perfil</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                        className="editProfile_modal"
+                    >
+                        {body}
+                    </Modal>
+                </div>
+                <div className="profile_description">
+                    <h3>{userData.displayName}</h3>
+                    <span>@{userData.username}</span>
+                    <pan>{userData.description}</pan>
+                </div>
             </div>
         </div>
     )
